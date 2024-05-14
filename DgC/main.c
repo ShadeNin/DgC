6452c4790a495de43388427a26244ab77b3e5486
Rodrigo Marques <rodrigomarquesmatos@hotmail.com>
2024-05-14 17:04:51 -0300

    fase 2 feita

--- DgC/main.c
+++ DgC/main.c
@@ -7,9 +7,10 @@ void tutorial();
 void fase1();
 void derrota();
 void vitoria();
+void fase2();
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int pvida = 1;
+int pvida = 3;
 
 void tutorial(){ //função para rodar o tutorial quando selecionado no menu inicial
 	char tutorial[21][51];
@@ -155,7 +156,6 @@ void fase1(){
 	int inimov;
 	int inival;
 	
-	pvida = 1;
 	pchave1 = 0;
 	pchave2 = 0;
 	player = '&';
@@ -172,6 +172,12 @@ void fase1(){
 	srand(time(NULL));
 	
 	do{
+		//0 vidas, game over
+		if(pvida == 0){
+			derrota();
+			return;
+		}
+		
 			if(pchave1!=0){
 			porta1 = '=';
 		}else{
@@ -214,7 +220,9 @@ void fase1(){
 		}
 		
 		if(py==iy&&px==ix){
-			pvida = 0;
+			pvida--;
+			fase1();
+			return;
 		}
 		
 		tecla = getch();
@@ -225,7 +233,9 @@ void fase1(){
     		        mapa1[py][px] = ' ';
     		        py--;
     		        if(mapa1[py][px] == inimigo1 || mapa1[py][px] == espinho){
-    		            pvida--;  
+   		            pvida--;
+					fase1();
+					return;  
     		        }
     		        mapa1[py][px] = player;
    		     } else {
@@ -238,7 +248,9 @@ void fase1(){
      		       mapa1[py][px] = ' ';
       		      py++;
      		       if(mapa1[py][px] == inimigo1 || mapa1[py][px] == espinho){
-     		           pvida--; 
+     		           pvida--;
+					fase1();
+					return; 
     		        }
     		        mapa1[py][px] = player;
     		    } else {
@@ -251,7 +263,9 @@ void fase1(){
     		        mapa1[py][px] = ' ';
     		        px--;
     		        if(mapa1[py][px] == inimigo1 || mapa1[py][px] == espinho){
-    		            pvida--;  
+   		            pvida--;
+					fase1();
+					return;  
     		        }
    		         mapa1[py][px] = player;
     		    } else {
@@ -264,7 +278,9 @@ void fase1(){
     		        mapa1[py][px] = ' ';
     		        px++;
       		      if(mapa1[py][px] == inimigo1 || mapa1[py][px] == espinho){
-      		          pvida--;  
+      		        pvida--;
+					fase1();
+					return;  
       		      }
      		       mapa1[py][px] = player;
      		   } else {
@@ -288,9 +304,11 @@ void fase1(){
      		derrota();
      		return;
 		}
+		//define se vai mudar eixo X ou Y e se vai aumentar ou diminuir um valor
 		inimov = rand()%2;
 		inival = rand()%2;
 		
+		//movimento do inimigo
 		if(inimov==0){
 			if(inival==0&&mapa1[iy][ix+1]!='*'&&mapa1[iy][ix+1]!='D'&&mapa1[iy][ix+1]!='@'){
 				ix++;
@@ -313,7 +331,7 @@ void fase1(){
 		
 		if(mapa1[6][0]!='D'&&mapa1[6][0]!='*'){
 				if(mapa1[py][px]==mapa1[6][0]){
-					vitoria();
+					fase2();
 					return;
 				}
 		}
@@ -362,6 +380,7 @@ void derrota(){
 				break;
 			case 13:
 				if(opcao==1){
+					pvida = 3;
 					fase1();
 					return;
 				}else if(opcao==2){
@@ -390,6 +409,283 @@ void vitoria(){
 		
 		tecla = getch();
 	}while(tecla!=13);
+}
+
+void fase2(){
+	//declaração das variaveis
+	int px;
+	int py;
+	int i1x;
+	int i1y;
+	int i2x;
+	int i2y;
+	int i3x;
+	int i3y;
+	char player;
+	char inimigo1;
+	char inimigo2;
+	char inimigo3;
+	char chave1;
+	char porta1;
+	char chave2;
+	char porta2;
+	char chave3;
+	char porta3;
+	char mapa[20][20];
+	char botao;
+	char espinho1;
+	char espinho2;
+	char tecla;
+	int i;
+	int j;
+	int pchave1;
+	int pchave2;
+	int inimov;
+	int inival;
+	
+	//Valor para as variaveis
+	porta1 = 'D';
+	porta2 = 'D';
+	porta3 = 'D';
+	chave1 = '@';
+	chave2 = '@';
+	chave3 = '@';
+	espinho1 = '#';
+	espinho2 = '#';
+	botao = 'O';
+	player = '&';
+	inimigo1 = 'X';
+	inimigo2 = 'Y';
+	inimigo3 = 'X';
+	px = 5;
+	py = 4;
+	i1x = 4;
+	i1y = 2;
+	i2x = 17;
+	i2y = 5;
+	i3x = 9;
+	i3y = 12;
+	pchave1 = 0;
+	pchave2 = 0;
+	srand(time(NULL));
+	
+	do{
+		//se o player perde todas as vidas, game over
+		if(pvida == 0){
+			derrota();
+			return;
+		}
+		
+		//quando o player aperta o botao, os espinhos somem
+		if(py == 1 && px == 1){
+			espinho1 = ' ';
+		}//quando player pega chave 1, porta 1 abre
+		if(pchave1 != 0){
+			porta1 = '=';
+		}//quando player pega a chave 2, porta 2 abre
+		if(pchave2 != 0){
+			porta2 = '=';
+		}
+		
+		//apaga a tela e depois imprime o novo mapa com os novos dados
+		system("cls");
+		for(i = 0; i < 20; i++){
+			for(j = 0; j < 20; j++){
+				mapa[i][j] = ' ';
+				if(i == 0||i == 19){
+					mapa[i][j] = '*';
+				}else if(j == 0||j == 19){
+					mapa[i][j] = '*';
+				}else if( i != 0 && i < 11){
+					mapa[i][10] = espinho1;
+				}else if(j != 0 && j != 19){
+					mapa[11][j] = '*';
+				}
+				//objetos dentro do mapa
+				mapa[1][1] = botao;
+				mapa[19][10] = porta2;
+				mapa[11][15] = porta1;
+				mapa[9][18] = chave1;
+				mapa[17][1] = chave2;
+				mapa[py][px] = player;
+				mapa[i1y][i1x] = inimigo1;
+				mapa[i2y][i2x] = inimigo2;
+				mapa[i3y][i3x] = inimigo3;
+			}
+		}
+		
+		for(i = 0; i < 20; i++){
+			printf("\t\t");
+			for(j = 0; j < 20; j++){
+				printf("%c ", mapa[i][j]);
+			}
+			printf("\n");
+		}
+		
+		//captura o movimento do jogador
+		tecla = getch();
+		
+		//dependendo da tecla apertada, se move em uma direção
+		switch(tecla){
+			case 119:// W
+				if(mapa[py-1][px]!='*'&&mapa[py-1][px]!='D'&&mapa[py-1][px]!='@'){
+					py--;
+					if(mapa[py][px]=='#'){
+						pvida--;
+						fase2();
+						return;
+					}
+				}else{
+					mapa[py][px] = player;
+				} 
+				break;
+			case 115://S
+				if(mapa[py+1][px]!='*'&&mapa[py+1][px]!='D'&&mapa[py+1][px]!='@'){
+					py++;
+					if(mapa[py][px]=='#'){
+						pvida--;
+						fase2();
+						return;
+					}
+				}else{
+					mapa[py][px] = player;
+				}
+				break;
+			case 100://D
+				if(mapa[py][px+1]!='*'&&mapa[py][px+1]!='D'&&mapa[py][px+1]!='@'){
+					px++;
+					if(mapa[py][px]=='#'){
+						pvida--;
+						fase2();
+						return;
+					}
+				}else{
+					mapa[py][px] = player;
+				}
+				break;
+			case 97://A
+				if(mapa[py][px-1]!='*'&&mapa[py][px-1]!='D'&&mapa[py][px-1]!='@'){
+					px--;
+					if(mapa[py][px]=='#'){
+						pvida--;
+						fase2();
+						return;
+					}
+				}else{
+					mapa[py][px] = player;
+				}
+				break;
+			case 105://I
+				if(mapa[py-1][px] == chave1){
+					pchave1 = 1;
+					chave1 = ' ';
+				}else if(mapa[py-1][px] == chave2){
+					pchave2 = 1;
+					chave2 = ' ';
+				}else{
+					mapa[py][px] = player;
+				}
+				break;
+		}
+		
+		//Condição para vitória
+		if(mapa[py][px] == mapa[19][10]){
+			vitoria();
+			return;
+		}
+		
+		//Condição de derrota do player para mostro
+		if(py == i1y&&px == i1x){
+			pvida--;
+			fase2();
+			return;
+		}
+		if(py == i2y&&px == i2x){
+			pvida--;
+			fase2();
+			return;
+		}
+		if(py == i3y&&px == i3x){
+			pvida--;
+			fase2();
+			return;
+		}
+		
+		
+		//define se vai mudar eixo X ou Y e se vai aumentar ou diminuir um valor do inimigo 1
+		inimov = rand()%2;
+		inival = rand()%2;
+		
+		//movimento do inimigo 1
+		if(inimov==0){
+			if(inival==0&&mapa[i1y][i1x+1]!='*'&&mapa[i1y][i1x+1]!='D'&&mapa[i1y][i1x+1]!='@'&&mapa[i1y][i1x+1]!='#'){
+				i1x++;
+			}else if(inival!=0&&mapa[i1y][i1x-1]!='*'&&mapa[i1y][i1x-1]!='D'&&mapa[i1y][i1x-1]!='@'&&mapa[i1y][i1x-1]!='#'){
+				i1x--;
+			}else{
+				mapa[i1y][i1x]=inimigo1;
+			}
+		}else{
+			if(inival==0&&mapa[i1y+1][i1x]!='*'&&mapa[i1y+1][i1x]!='D'&&mapa[i1y+1][i1x]!='@'&&mapa[i1y+1][i1x]!='#'){
+				i1y++;
+			}else if(inival!=0&&mapa[i1y-1][i1x]!='*'&&mapa[i1y-1][i1x]!='D'&&mapa[i1y-1][i1x]!='@'&&mapa[i1y-1][i1x]!='#'){
+				i1y--;
+			}else{
+				mapa[i1y][i1x]=inimigo1;
+			}
+		}
+		
+		//define se vai mudar eixo X ou Y e se vai aumentar ou diminuir um valor do inimigo 3
+		inimov = rand()%2;
+		inival = rand()%2;
+		
+		//movimento do inimigo 3
+		if(inimov==0){
+			if(inival==0&&mapa[i3y][i3x+1]!='*'&&mapa[i3y][i3x+1]!='D'&&mapa[i3y][i3x+1]!='@'&&mapa[i3y][i3x+1]!='#'){
+				i3x++;
+			}else if(inival!=0&&mapa[i3y][i3x-1]!='*'&&mapa[i3y][i3x-1]!='D'&&mapa[i3y][i3x-1]!='@'&&mapa[i3y][i3x-1]!='#'){
+				i3x--;
+			}else{
+				mapa[i3y][i3x]=inimigo3;
+			}
+		}else{
+			if(inival==0&&mapa[i3y+1][i3x]!='*'&&mapa[i3y+1][i3x]!='D'&&mapa[i3y+1][i3x]!='@'&&mapa[i3y+1][i3x]!='#'){
+				i3y++;
+			}else if(inival!=0&&mapa[i3y-1][i3x]!='*'&&mapa[i3y-1][i3x]!='D'&&mapa[i3y-1][i3x]!='@'&&mapa[i3y-1][i3x]!='#'){
+				i3y--;
+			}else{
+				mapa[i3y][i3x]=inimigo3;
+			}
+		}
+		
+		//mov do inimigo 2
+		if(py!=i2y&&px!=i2x&&tecla!=105){
+			inimov = rand()%2;
+		}else if(py == i2y&&tecla!=105){
+			inimov = 0;
+		}else if(px == i2x&&tecla!=105){
+			inimov = 1;
+		}else if(tecla = 105){
+			inimov = 3;
+		}
+		
+		//inimigo 2 seguindo o player
+		if(inimov == 0){
+			if(px>i2x&&mapa[i2y][i2x+1]!='D'&&mapa[i2y][i2x+1]!='*'&&mapa[i2y][i2x+1]!='#'&&mapa[i2y][i2x+1]!='@'){
+				i2x++;
+			}else if(px<i2x&&mapa[i2y][i2x-1]!='D'&&mapa[i2y][i2x-1]!='*'&&mapa[i2y][i2x-1]!='#'&&mapa[i2y][i2x-1]!='@'){
+				i2x--;
+			}
+		}else if(inimov == 1){
+			if(py>i2y&&mapa[i2y+1][i2x]!='D'&&mapa[i2y+1][i2x]!='#'&&mapa[i2y+1][i2x]!='@'&&mapa[i2y+1][i2x]!='*'){
+				i2y++;
+			}else if(py<i2y&&mapa[i2y-1][i2x]!='D'&&mapa[i2y-1][i2x]!='@'&&mapa[i2y-1][i2x]!='#'&&mapa[i2y-1][i2x]!='*'){
+				i2y--;
+			}
+		}
+		
+	}while(tecla!=27||pvida!=pvida--);//ESC para sair
+	
 }
 
 int main(int argc, char *argv[]) {
@@ -453,7 +749,7 @@ int main(int argc, char *argv[]) {
 					case 1:
 						printf("Que comece a jornada!");
 						sleep (3);
-						fase1();
+						fase2();
 						break;
 					case 2:
 						printf("Iniciando o tutorial!");
